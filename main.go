03c2090de60801477d203553338123f10cb18b90
Jonathan Manera <manerajona@gmail.com>
2019-09-15 21:47:47 -0300

    style

--- main.go
+++ main.go
@@ -27,32 +27,34 @@ func partition(arr []int, left int, right int) int {
 			arr[j] = temp
 		}
 	}
-	temp := arr[i+1]
-	arr[i+1] = arr[right]
+	i++
+	temp := arr[i]
+	arr[i] = arr[right]
 	arr[right] = temp
 
-	return i + 1
+	return i
 }
 
 var testData = []struct {
-	input          []int
-	expectedOutput []int
-	left, right    int
+	input, expectedOutput []int
+	left, right           int
 }{
 	{[]int{}, []int{}, 0, 0},
-	{[]int{42}, []int{42}, 0, 0},
-	{[]int{42, 23}, []int{23, 42}, 0, 1},
-	{[]int{23, 42, 32, 64, 12, 4}, []int{4, 12, 23, 32, 42, 64}, 0, 5},
+	{[]int{1}, []int{1}, 0, 0},
+	{[]int{1, 1}, []int{1, 1}, 0, 1},
+	{[]int{1, 2, 1}, []int{1, 1, 2}, 0, 2},
+	{[]int{13, 8, 5, 1, 1, 2, 3}, []int{1, 1, 2, 3, 5, 8, 13}, 0, 6},
 }
 
 func main() {
 	for _, testCase := range testData {
 		actual := quicksort(testCase.input, testCase.left, testCase.right)
 		expected := testCase.expectedOutput
-		fmt.Printf("Quicksorted : %v \n", actual)
 
 		if !reflect.DeepEqual(actual, expected) {
-			fmt.Printf("%v != %v\n", actual, expected)
+			fmt.Printf("Test failed: %v != %v\n", actual, expected)
+		} else {
+			fmt.Printf("Quicksorted: %v \n", actual)
 		}
 	}
 }
